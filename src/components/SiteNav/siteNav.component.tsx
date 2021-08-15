f09320e487b6028c3af11c790ea53291bba1274e
Garry Tsaconas <garrytsaconas@insightactioninc.com>
2021-08-15 15:21:05 -0400

    added mobile menu

--- src/components/SiteNav/siteNav.component.tsx
+++ src/components/SiteNav/siteNav.component.tsx
@@ -1,19 +1,45 @@
+import { useState } from "react";
+
 const SiteNav = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false);
+
   return (
-    <nav className="siteNav">
-      <ul>
-        <li title="NFL" className="selected">
-          NFL
-        </li>
-        <li title="NFL">NHL</li>
-        <li title="NBA">NBA</li>
-        <li title="Soccer">Soccer</li>
-        <li title="NCAAF">NCAAF</li>
-        <li title="Wiffle Ball">Wiffle Ball</li>
-        <li title="Lawn Darts">Lawn Darts</li>
-        <li title="Square Dancing">Square Dancing</li>
-      </ul>
-    </nav>
+    <>
+      <nav className="siteNav">
+        <ul>
+          <li title="NFL" className="selected">
+            NFL
+          </li>
+          <li title="NFL">NHL</li>
+          <li title="NBA">NBA</li>
+          <li title="Soccer">Soccer</li>
+          <li title="NCAAF">NCAAF</li>
+          <li title="Wiffle Ball">Wiffle Ball</li>
+          <li title="Lawn Darts">Lawn Darts</li>
+          <li title="Square Dancing">Square Dancing</li>
+        </ul>
+      </nav>
+      <span
+        className="siteNav-mobile-menu-toggle"
+        onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+      />
+      <nav
+        className={`siteNav siteNav-mobile ${isMobileMenuOpen ? "--open" : ""}`}
+      >
+        <ul>
+          <li title="NFL" className="selected">
+            NFL
+          </li>
+          <li title="NFL">NHL</li>
+          <li title="NBA">NBA</li>
+          <li title="Soccer">Soccer</li>
+          <li title="NCAAF">NCAAF</li>
+          <li title="Wiffle Ball">Wiffle Ball</li>
+          <li title="Lawn Darts">Lawn Darts</li>
+          <li title="Square Dancing">Square Dancing</li>
+        </ul>
+      </nav>
+    </>
   );
 };
 
